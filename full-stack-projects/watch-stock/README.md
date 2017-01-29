cfcef47317b14723f5df6ffb9da7e359b2cdd5b8
arshad <arshdkhn1@gmail.com>
2017-01-30 05:11:13 +0530

    addin new proj

---
+++ full-stack-projects/watch-stock/README.md
@@ -0,0 +1,10 @@
+# stock-market-app
+A full stack js app for stock-market enthusiasts.
+
+
+## todos
+* add notification feature - I
+* add a feature like `a moving average per week over 6 months` for stocks to present a more meaningful data.
+
+## problems
+* outline question
---
+++ full-stack-projects/watch-stock/client/src/components/single-stock/stock.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import {socketConnect} from 'socket.io-react';
+
+class Stock extends Component {
+
+  emitRemoveEvent(code) {
+    this.props.socket.emit('removeStock', code);
+  }
+
+  render() {
+    return (
+      <div
+        className={
+          (this.props.hide ? "hide ": "") + "stockWrapper marB"
+        }
+      >
+        <div className="stock">
+          <h4>{this.props.code}</h4>
+          <p>{this.props.description}</p>
+          <button className="removeStockBtn"
+            ref={(node) => {this.removeButton = node;}}
+            onClick={(e) => {
+              e.target.parentNode.style.opacity = ".8";
+              this.emitRemoveEvent(this.props.code);
+            }}
+          >
+            x
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default socketConnect(Stock);
